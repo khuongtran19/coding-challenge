e56862e3b2443b4cdeb4d50a3eedf212dfbac9a8
khuong <tranvinhkhuong@gmail.com>
2020-02-14 14:17:24 -0800

    new challenge

---
+++ leetcode-challenge/189. Rotate Array/rotate-array.test.js
@@ -0,0 +1,13 @@
+const rotate = require('./rotate-array');
+
+test.each(
+    [
+        [[1, 2, 3, 4, 5, 6, 7], 3, [5, 6, 7, 1, 2, 3, 4]],
+        [[-1, -100, 3, 99], 2, [3, 99, -1, -100]],
+        [[1, 2], 1, [1]],
+        [[1, 2, 3], 4, [3, 1, 2]]
+    ]
+)
+    ('Given array [%s] and k = %i the result should be [%s]', (givenA, givenB, expected) => {
+        expect(rotate(givenA, givenB)).toEqual(expect.arrayContaining(expected));
+    })
