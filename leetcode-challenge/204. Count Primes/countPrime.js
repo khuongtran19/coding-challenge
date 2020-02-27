ed1be57e64a86a6b23b88e024424da49d0acf8d2
khuong <tranvinhkhuong@gmail.com>
2020-02-26 22:33:50 -0800

    new challenges

--- leetcode-challenge/204. Count Primes/countPrime.js
+++ leetcode-challenge/204. Count Primes/countPrime.js
@@ -6,9 +6,12 @@
 // Output: 4
 // Explanation: There are 4 prime numbers less than 10, they are 2, 3, 5, 7.
 var countPrimes = function (n) {
+    //Approach by create new empty array and start the count at 0.
     let count = 0, newArr = []
     for (let i = 2; i < n; i++) {
+        //using for loop to loop through the original array.
         if (newArr[i] === undefined) {
+            //if there is undefined then we replace it with 1
             newArr[i] = 1
             count++;
             let j = 2;
@@ -17,7 +20,6 @@ var countPrimes = function (n) {
                 j++;
             }
         }
-
     }
     return count
 };
