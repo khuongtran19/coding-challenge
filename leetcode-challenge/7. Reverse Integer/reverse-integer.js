a29da6b6038cd90e7eec58c98e2af3a3b1ba1532
khuong <tranvinhkhuong@gmail.com>
2020-02-28 11:55:53 -0800

    update comment

--- leetcode-challenge/7. Reverse Integer/reverse-integer.js
+++ leetcode-challenge/7. Reverse Integer/reverse-integer.js
@@ -11,16 +11,18 @@
 // Input: 120
 // Output: 21
 // Note:
-// Assume we are dealing with an environment which could only store integers within the 32-bit signed integer range: [−231,  231 − 1]. For the purpose of this problem, assume that your function returns 0 when the reversed integer overflows.
+// Assume we are dealing with an environment which could only store integers within the 32-bit signed integer range: [−2^31,  2^31 − 1]. For the purpose of this problem, assume that your function returns 0 when the reversed integer overflows.
 
 // /**
 //  * @param {number} x
 //  * @return {number}
 //  */
 var reverse = function(x) {
-    if (x < 0) return -1 * reverse(-x);
+    if (x < 0) return -1 * reverse(-x); //if the given number is negative number then we use -1 to keep the negative. However we still use recursive to call back the function.
     const r = parseInt(`${x}`.split('').reverse().join(''), 10);
+    //from here we use normal .split .reverse and .join to reverse the number.
     if (r > 2 ** 31 - 1) return 0;
+    //The last case is when number smaller then -2^31 or larger then 2^31-1 as the problem require we simply return 0. 
 
     return r;
 };
