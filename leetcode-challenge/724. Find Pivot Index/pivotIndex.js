fe79e190f688bf9b87eef632e6bede858b250189
khuong <tranvinhkhuong@gmail.com>
2020-03-10 12:47:45 -0700

    new challenge

--- leetcode-challenge/724. Find Pivot Index/pivotIndex.js
+++ leetcode-challenge/724. Find Pivot Index/pivotIndex.js
@@ -45,4 +45,11 @@ var pivotIndex = function (nums) {
     return -1
 };
 
+//#2
+// var pivotIndex = function(nums) {
+//     for(let sum = nums.reduce((sum, num) => sum + num, 0), i = 0, leftSum = 0; i < nums.length; leftSum += nums[i], i++)
+//         if(leftSum === sum - nums[i] - leftSum) return i;
+//     return -1;
+// };
+
 module.exports = pivotIndex
