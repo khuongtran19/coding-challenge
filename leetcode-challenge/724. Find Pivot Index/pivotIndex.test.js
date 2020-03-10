2adabdc841951f7267a98d5a23ff4ac782e0e9a2
khuong <tranvinhkhuong@gmail.com>
2020-03-10 12:44:23 -0700

    new challenge

---
+++ leetcode-challenge/724. Find Pivot Index/pivotIndex.test.js
@@ -0,0 +1,11 @@
+const pivotIndex = require("./pivotIndex");
+
+test.each(
+    [
+        [[1, 7, 3, 6, 5, 6], 3],
+        [[1, 2, 3], -1]
+    ]
+)
+    ('Given array "%s" and the pivot index should be %i', (given, expected) => {
+        expect(pivotIndex(given)).toEqual(expected);
+    })
