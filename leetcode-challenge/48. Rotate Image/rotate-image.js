78d37c70668fca33e4490346edac3e9884651b8a
khuong <tranvinhkhuong@gmail.com>
2020-03-10 21:00:24 -0700

    new challenge

--- leetcode-challenge/48. Rotate Image/rotate-image.js
+++ leetcode-challenge/48. Rotate Image/rotate-image.js
@@ -45,19 +45,35 @@
 //  */
 
 var rotate = function (matrix) {
-    // To solve this problem we need to look at the matrix as vertical (y) and horizontal (x) position 
+    // To solve this problem we need to look at the matrix as vertical (y) and horizontal (x) position
+    // we use matrix[x][y] to solve this problem
+    // [0,0], [0,1], [0,2], [0,3]
+    // [1,0], [1,1], [1,2], [1,3]
+    // [2,0], [2,1], [2,2], [2,3]
+    // [3,0], [3,1], [3,2], [3,3]
     const n = matrix.length;
-    const x = Math.floor(n / 2); // x is horizontal meaning all number in the same inner array
-    const y = n - 1; // y is vertical meaning all number in same index of the array
+    // length of each inner array in this case [5,1,9,11] => matrix.length = 4
+    // we don't know if ther problem give us more than 4 item in the inner array
+    // simply call matrix.length to let program check for us
+    const x = Math.floor(n / 2);
+    // x is horizontal meaning all number in the same inner array
+    // Math.floor will gave us the integer less than or equal give number
+    // In this case it x = 4/2 = 2
+    const y = n - 1;
+    // y is vertical meaning all number in same index of the array
     for (let i = 0; i < x; i++) {
+        // the i in this for loop will be all number in the inner array
+        // start at position 0
         for (let j = i; j < y - i; j++) {
+            // the j in this for loop will be all number have same index of each inner array
+            // in this case we have first number is 5 
             k = matrix[i][j];
             // We move one by one each number position 
             //   _________
             //  |         |
             //   --5 1 9 11      move number 5 to position of number 11
-            matrix[i][j] = matrix[y - j][i];
-            matrix[y - j][i] = matrix[y - i][y - j];
+            matrix[i][j] = matrix[y - j][i]; // matrix[0][0] = matrix[0-1][0]
+            matrix[y - j][i] = matrix[y - i][y - j]; // 
             matrix[y - i][y - j] = matrix[j][y - i];
             matrix[j][y - i] = k
         }
