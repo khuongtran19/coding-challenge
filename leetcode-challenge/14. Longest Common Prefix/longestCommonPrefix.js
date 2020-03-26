214e321f4454638cfd6cf337c05ff96c28598099
khuong <tranvinhkhuong@gmail.com>
2020-03-25 17:26:22 -0700

    new challenge

--- leetcode-challenge/14. Longest Common Prefix/longestCommonPrefix.js
+++ leetcode-challenge/14. Longest Common Prefix/longestCommonPrefix.js
@@ -23,22 +23,29 @@ var longestCommonPrefix = function (strs) {
     if (strs.length === 0) return '';
     // if there is no words or empty string then return empty
 
-    // First scan: find shortest length
     let minLength = strs[0].length;
+    // minLength as the first letter of the word we looking
+
+    // First scan: find shortest length
     for (const str of strs) {
+        // check each word as "str" in the array
         if (str.length < minLength) {
             minLength = str.length;
         }
     }
 
-    // Second scan: find common chars until a differetn one
+    // Second scan: find common chars until a different one
     for (let i = 0; i < minLength; i++) {
+        // check every letter of each word in the array
         for (const str of strs) {
             if (str[i] !== strs[0][i]) {
+                // if the letter of current checking word not same as the letter of the next word
                 return str.substr(0, i);
+                // use substr to get from the first letter to the last letter that both word have the same
             }
         }
     }
+
     return strs[0].substr(0, minLength);
 };
 
