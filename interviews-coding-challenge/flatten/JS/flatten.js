364a16bc63c91210f926f5294b38b8d4a5ecd556
khuong <tranvinhkhuong@gmail.com>
2020-05-26 19:26:52 -0700

    new challgenge

---
+++ interviews-coding-challenge/flatten/JS/flatten.js
@@ -0,0 +1,14 @@
+function flatten(arr) {
+
+    let newArr = [];
+    for (let i = 0; i < arr.length; i++) {
+        if (Array.isArray(arr[i])) {
+            newArr = newArr.concat(flatten(arr[i]));
+        } else {
+            newArr.push(arr[i]);
+        }
+    }
+    return newArr;
+}
+
+module.exports = flatten
