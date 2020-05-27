0f6bdce6fdfcd2f75bd45e03da59e55d6c8dbacc
khuong <tranvinhkhuong@gmail.com>
2020-05-26 19:10:53 -0700

    new challgenge

--- interviews-coding-challenge/someRecursive/JS/someRecursive.test.js
+++ interviews-coding-challenge/someRecursive/JS/someRecursive.test.js
@@ -1,11 +1,17 @@
 const someRecursive = require('./someRecursive');
 
+function isOdd(num) {
+    return num % 2 === 1;
+}
+
 test.each(
     [
         [[1, 2, 3, 4], isOdd, true],
-        [[4, 6, 8, 9], isOdd, true]
+        [[4, 6, 8, 9], isOdd, true],
+        [[4, 6, 8], isOdd, false],
+        [[4, 6, 8], val => val > 10, false]
     ]
 )
-    ('Given [%s] and %s result shoudl be %i', (givenA, givenB, expected) => {
+    ('Given [%s] and %s result should be %i', (givenA, givenB, expected) => {
         expect(someRecursive(givenA, givenB)).toEqual(expected);
     })
