c57dfa07fc523f1bcaba9f0037889d4699e8aa0a
khuong <tranvinhkhuong@gmail.com>
2020-05-28 02:27:15 -0700

    new challenge

---
+++ interviews-coding-challenge/BinarySearch/JS/binarySearch.js
@@ -0,0 +1,15 @@
+// Write a function called binarySearch which accepts a sorted array and a value and returns the index at which the value exists Otherwise, return -1
+
+// Example:
+// binarySearch([1,2,3,4,5], 2)
+function binarySearch(arr, val) {
+    let l = 0;
+    let r = arr.length - 1;
+    let m = Math.floor((l + r) / 2);
+    while (arr[m] !== val && l <= r) {
+        if (val < arr[m]) r = m - 1;
+        else l = m + 1;
+        m = Math.floor((l + r) / 2);
+    }
+    return arr[m] === val ? m : -1;
+}
