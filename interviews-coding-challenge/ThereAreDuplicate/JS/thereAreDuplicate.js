e9bc0093046ad1ae55d82ad605ab92c2fc1eb945
khuong <tranvinhkhuong@gmail.com>
2020-05-28 18:18:56 -0700

    new challenge

--- interviews-coding-challenge/ThereAreDuplicate/JS/thereAreDuplicate.js
+++ interviews-coding-challenge/ThereAreDuplicate/JS/thereAreDuplicate.js
@@ -6,17 +6,20 @@
 // areThereDuplicates('a','b','c','a') //true
 
 // Time O(n)
-
 function areThereDuplicates(arr) {
+    // To get the linear time complexity we simply making object contain how many times the word or number appear in the given array.
     let fre = {};
     for (let i in arr) {
         fre[arr[i]] = (fre[arr[i]] || 0) + 1;
+        // if there is number or word already in the object we add 1 otherwise we have our first
     }
     for (let key in arr) {
         if (fre[arr[key]] > 1) {
+            // check if there are number or word show up more than 1 times meaning we have duplicate
             return true;
         }
     }
+    // Otherwise return false if there is no word or number have number larger than 1
     return false;
 }
 
